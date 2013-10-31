5342a523b46c6ca35f3e9571ba903f60dfd3fc5b
Mathieu Lajugie <mathieu@caring.com>
2013-10-31 12:15:16 -0700

    fix table view controller

--- Albums/Albums/ZLAlbumsViewController.m
+++ Albums/Albums/ZLAlbumsViewController.m
@@ -57,7 +57,7 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     static NSString *CellIdentifier = @"Cell";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
     
     if (cell == nil) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
