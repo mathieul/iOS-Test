ba9d6b907885ef79ae9951eeba82dd4a0a27dfa0
Mathieu Lajugie <mathieul@gmail.com>
2013-11-03 12:23:27 -0800

    wip: testing push behavior

--- Gravity/Gravity/ViewController.m
+++ Gravity/Gravity/ViewController.m
@@ -11,9 +11,10 @@
 NSString *const kBottomBoundary = @"bottomBoundary";
 
 
-@interface ViewController () <UICollisionBehaviorDelegate>
-@property (nonatomic, strong) NSMutableArray *squareViews;
+@interface ViewController ()
+@property (nonatomic, strong) UIView *squareView;
 @property (nonatomic, strong) UIDynamicAnimator *animator;
+@property (nonatomic, strong) UIPushBehavior *pushBehavior;
 @end
 
 @implementation ViewController
@@ -43,65 +44,37 @@ NSString *const kBottomBoundary = @"bottomBoundary";
 {
     [super viewDidAppear:animated];
     
-    NSUInteger const NumberOfViews = 2;
-    
-    self.squareViews = [[NSMutableArray alloc] initWithCapacity:NumberOfViews];
-    NSArray *colors = @[[UIColor redColor], [UIColor greenColor]];
-    
-    CGPoint currentCenterPoint = self.view.center;
-    currentCenterPoint.y = 100.0f;
-    CGSize eachViewSize = CGSizeMake(50.0f, 50.0f);
-    for (NSUInteger counter = 0; counter < NumberOfViews; counter++) {
-        UIView *newView = [[UIView alloc] initWithFrame:
-                           CGRectMake(0.0f, 0.0f, eachViewSize.width, eachViewSize.height)];
-        newView.backgroundColor = colors[counter];
-        newView.center = currentCenterPoint;
-        currentCenterPoint.y += eachViewSize.height + 10;
-        [self.view addSubview:newView];
-        [self.squareViews addObject:newView];
-    }
-
-
-    self.animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
-    UIGravityBehavior *gravity = [[UIGravityBehavior alloc] initWithItems:self.squareViews];
-    [self.animator addBehavior:gravity];
-    
-    UICollisionBehavior *collision = [[UICollisionBehavior alloc] initWithItems:self.squareViews];
-    collision.collisionMode = UICollisionBehaviorModeBoundaries;
-    CGFloat y = self.view.bounds.size.height - 50.0f;
-    [collision addBoundaryWithIdentifier:@"bottomBoundary"
-                               fromPoint:CGPointMake(0.0f, y)
-                                 toPoint:CGPointMake(self.view.bounds.size.width, y)];
-    collision.collisionDelegate = self;
-    [self.animator addBehavior:collision];
+    [self createSmallSquareView];
+    [self createGestureRecognizer];
+    [self createAnimatorAndBehaviors];
 }
 
-- (void)collisionBehavior:(UICollisionBehavior*)paramBehavior
-      beganContactForItem:(id <UIDynamicItem>)paramItem
-   withBoundaryIdentifier:(id <NSCopying>)paramIdentifier
-                  atPoint:(CGPoint)paramPoint
+- (void)createSmallSquareView
 {
-    NSString *identifier = (NSString *)paramIdentifier;
-
-    if ([identifier isEqualToString:kBottomBoundary]) {
-        [UIView animateWithDuration:1.0f animations:^{
-            UIView *view = (UIView *)paramItem;
-            view.backgroundColor = [UIColor redColor];
-            view.alpha = 0.0f;
-            view.transform = CGAffineTransformMakeScale(2.0f, 2.0f);
-        } completion:^(BOOL finished){
-            UIView *view = (UIView *)paramItem;
-            [paramBehavior removeItem:paramItem];
-            [view removeFromSuperview];
-        }];
-    }
+    self.squareView = [[UIView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 80.0f, 80.0f)];
+    self.squareView.backgroundColor = [UIColor greenColor];
+    self.squareView.center = self.view.center;
+    [self.view addSubview:self.squareView];
 }
 
-//- (void)collisionBehavior:(UICollisionBehavior*)behavior
-//      endedContactForItem:(id <UIDynamicItem>)item
-//   withBoundaryIdentifier:(id <NSCopying>)identifier
-//{
-//    
-//}
+- (void)createGestureRecognizer
+{
+    UITapGestureRecognizer *tapGestureRecognizer =
+        [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTap:)];
+    [self.view addGestureRecognizer:tapGestureRecognizer];
+}
+
+- (void)createAnimatorAndBehaviors
+{
+    self.animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
+    UICollisionBehavior *collision =
+        [[UICollisionBehavior alloc] initWithItems:@[self.squareView]];
+    collision.translatesReferenceBoundsIntoBoundary = YES;
+    [self.animator addBehavior:collision];
+    self.pushBehavior = [[UIPushBehavior alloc]
+                         initWithItems:@[self.squareView]
+                         mode:UIPushBehaviorModeContinuous];
+    [self.animator addBehavior:self.pushBehavior];
+}
 
 @end
